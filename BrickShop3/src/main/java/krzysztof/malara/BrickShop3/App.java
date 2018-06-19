9e5a6ac40b9c8bef47d938a7f676bbf632b1c929
Krzysztof Malara <malarastudy@gmail.com>
2018-06-19 20:28:00 +0100

    initial commit

---
+++ BrickShop3/src/main/java/krzysztof/malara/BrickShop3/App.java
@@ -0,0 +1,13 @@
+package krzysztof.malara.BrickShop3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
