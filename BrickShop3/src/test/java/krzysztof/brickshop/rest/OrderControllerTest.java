f99ef8005e7b28eb239dbe858ad041f46d7c92e8
Krzysztof Malara <malarastudy@gmail.com>
2018-06-23 20:02:46 +0100

    Stage 1 Many customers have submitted orders

--- BrickShop3/src/test/java/krzysztof/brickshop/rest/OrderControllerTest.java
+++ BrickShop3/src/test/java/krzysztof/brickshop/rest/OrderControllerTest.java
@@ -15,24 +15,23 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class OrderControllerTest {
-    private static final String ORDERS_KRZYSZTOF = "/orders/krzysztof";
+    private static final String KRZYSZTOF = "krzysztof";
+    private static final String JOHN = "john";
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void shouldCreateOrdersAndReturnUniqueOrderReference() throws Exception {
         // given - when
-        MvcResult result1 = performCreateOrder(100);
-        MvcResult result2 = performCreateOrder(200);
+        MvcResult result1 = performCreateOrder(KRZYSZTOF, 100);
+        MvcResult result2 = performCreateOrder(KRZYSZTOF, 200);
 
         // then
         String orderReferenceId1 = result1.getResponse().getContentAsString();
@@ -41,8 +40,8 @@ public class OrderControllerTest {
         assertThat(orderReferenceId1).isNotEqualTo(orderReferenceId2);
     }
 
-    private MvcResult performCreateOrder(int content) throws Exception {
-        return mockMvc.perform(post(ORDERS_KRZYSZTOF).content(Integer.toString(content)))
+    private MvcResult performCreateOrder(String user, int content) throws Exception {
+        return mockMvc.perform(post("/orders/" + user).content(Integer.toString(content)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$", notNullValue()))
@@ -54,14 +53,14 @@ public class OrderControllerTest {
     public void shouldGetSortedOrdersWithProperOrderReference() throws Exception {
         // given - when
         int brickCount1 = 100;
-        MvcResult result1 = performCreateOrder(brickCount1);
+        MvcResult result1 = performCreateOrder(KRZYSZTOF, brickCount1);
         int brickCount2 = 200;
-        MvcResult result2 = performCreateOrder(brickCount2);
+        MvcResult result2 = performCreateOrder(KRZYSZTOF, brickCount2);
 
         // then
         String orderReferenceId1 = result1.getResponse().getContentAsString();
         String orderReferenceId2 = result2.getResponse().getContentAsString();
-        mockMvc.perform(get(ORDERS_KRZYSZTOF)).andDo(print()).andExpect(status().isOk())
+        mockMvc.perform(get("/orders/" + KRZYSZTOF)).andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].orderReferenceId", is(orderReferenceId1)))
                 .andExpect(jsonPath("$[0].bricksCount", is(brickCount1)))
@@ -71,8 +70,39 @@ public class OrderControllerTest {
 
     @Test
     public void shouldReturnEmptyListForNotExistingUser() throws Exception {
-        mockMvc.perform(get(ORDERS_KRZYSZTOF)).andDo(print()).andExpect(status().isOk())
+        mockMvc.perform(get("/orders/" + KRZYSZTOF)).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("{}"));
+    }
+
+    @Test
+    public void shouldReturnOrdersWithReferencesAndBrickAmount() throws Exception {
+        //given - when
+        int brickCount1 = 100;
+        MvcResult result1 = performCreateOrder(KRZYSZTOF, brickCount1);
+        int brickCount2 = 200;
+        MvcResult result2 = performCreateOrder(KRZYSZTOF, brickCount2);
+        int brickCount3 = 300;
+        MvcResult result3 = performCreateOrder(JOHN, brickCount3);
+        int brickCount4 = 400;
+        MvcResult result4 = performCreateOrder(JOHN, brickCount4);
+
+        //then
+
+        String orderReferenceId1 = result1.getResponse().getContentAsString();
+        String orderReferenceId2 = result2.getResponse().getContentAsString();
+        String orderReferenceId3 = result3.getResponse().getContentAsString();
+        String orderReferenceId4 = result4.getResponse().getContentAsString();
+        mockMvc.perform(get("/orders/" + KRZYSZTOF)).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].orderReferenceId", is(orderReferenceId1)))
+                .andExpect(jsonPath("$[0].bricksCount", is(brickCount1)))
+                .andExpect(jsonPath("$[1].orderReferenceId", is(orderReferenceId2)))
+                .andExpect(jsonPath("$[1].bricksCount", is(brickCount2)));
+
+        mockMvc.perform(get("/orders/" + JOHN)).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].orderReferenceId", is(orderReferenceId3)))
+                .andExpect(jsonPath("$[0].bricksCount", is(brickCount3)))
+                .andExpect(jsonPath("$[1].orderReferenceId", is(orderReferenceId4)))
+                .andExpect(jsonPath("$[1].bricksCount", is(brickCount4)));
     }
 
 
